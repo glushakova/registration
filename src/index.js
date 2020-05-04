de6e7b4a2a7329e63f05b1b31d4839f40e845aba
glushakova <glushakovanastya@gmail.com>
2020-05-04 18:36:30 +0300

    all

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import firebaseConfig from './firebaseConfig';
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from 'firebase';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
